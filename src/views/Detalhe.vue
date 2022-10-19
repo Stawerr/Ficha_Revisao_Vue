481d89a5e380a8c2f42387b7b1da572b1a00965e
Stawerr <renato-silva_16@hotmail.com>
2022-10-19 16:32:55 +0100

    edit

--- src/views/Detalhe.vue
+++ src/views/Detalhe.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="border border-white d-flex flex-column p-5">
         <H1Comp text="Prática Laboratorial 8"></H1Comp>
-        <div class="d-flex flex-row justify-content-end align-items-end mt-4 gap-4">
-            <div><button class="border border-white bg-info rounded">Editar</button></div>
-            <div><button class="border border-white bg-danger rounded" @click="eliminar">Eliminar</button></div>
-        </div>
-        <div class="border border-white d-flex my-3 p-4">
-            <div class="d-flex flex-column align-items-start">
-                <p>{{$route.params.descricao}}</p>
-                <p>{{$route.params.estado}}</p>
-                <p>{{$route.params.data}}</p>
+        <form @submit.prevent="editar($event)">
+            <div class="d-flex flex-row justify-content-end align-items-end mt-4 gap-4">
+                <div><button class="border border-white bg-info rounded">Editar</button></div>
+                <div><button class="border border-white bg-danger rounded" @click="eliminar">Eliminar</button></div>
             </div>
-
-        </div>
+            <div class="border border-white d-flex my-3 p-4">
+                <div class="d-flex flex-column align-items-start">
+                    <input type="text" class="form-control" placeholder="Descrição da tarefa" ref="descricao" v-model="$route.params.descricao">
+                    <div class=" d-flex flex-row mt-4 gap-2">
+                        <input class="" type="radio" id="one" name="radioFalse" value="Por concluir" v-model="$route.params.estado" />
+                        <input class="" type="radio" id="two" name="radioTrue" value="Concluido" v-model="$route.params.estado" />
+                    </div>
+                    <p>{{$route.params.data}}</p>
+                </div>
+            
+            </div>
+        </form>
     </div>
 </template>
 
@@ -20,6 +25,7 @@
 import H1Comp from '../components/widgets/h1Comp.vue';
 import { mapState } from 'pinia';
 import {todoStore} from '../store/todoStore';
+import addTodo from '../models/addTodo';
 export default {
     setup(){
         // initialize the store
@@ -28,10 +34,19 @@ export default {
     },
     data() {
         return {
-
+            todo:{}
         };
     },
     methods: {
+        editar(){
+            this.todo={
+                id:this.$route.params.id,
+                descricao:this.$route.params.descricao,
+                estado:this.$route.params.estado,
+                data:this.$route.params.data}
+            this.todoStoreT.update(this.todo)
+            this.$router.push({ name: "Listagem" });
+        },
         eliminar() {
             this.todoStoreT.delete(this.$route.params.id)
             this.$router.push({ name: "Listagem" });
@@ -42,5 +57,10 @@ export default {
 </script>
 
 <style scoped>
-
+input[name="radioFalse"] {
+	accent-color: #DC3545;
+}
+input[name="radioTrue"] {
+	accent-color: #0DCAF0;
+}
 </style>
